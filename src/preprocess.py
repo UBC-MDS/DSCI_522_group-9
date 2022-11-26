a49961efd9899120518c9d61c645bc940bd0d85c
Ritisha Sharma <ritishas2000@gmail.com>
2022-11-26 12:33:33 -0800

    Add docstring and tests

--- src/preprocess.py
+++ src/preprocess.py
@@ -19,6 +19,23 @@ import json
 mappings = json.load(open("data_mapping.json", "r"))
 
 def main(in_path, preprocessed_out_dir, processed_out_dir):
+    """
+    Processes raw data into preprocessed and cleans to processed data
+    Parameters
+    ----------
+    in_path : string
+        path from where data is read
+    preprocessed_out_dir: string
+        path to the directory where preprocessed data is stored
+    processed_out_dir: string
+        path to the directory where processed data is stored
+    Returns
+    -------
+    None
+    Example
+    --------
+    main("../data/raw/drug_consumption.data", "../data/preocessed", "../data/processed")
+    """
     # df = pd.read_table("../data/raw/drug_consumption.data", index_col=0, names=column_names, delimiter=',')
     df = pd.read_table(in_path, index_col=0, names=mappings["column_headers"], delimiter=',')
 
@@ -30,10 +47,19 @@ def main(in_path, preprocessed_out_dir, processed_out_dir):
 
     # df.to_csv("../data/preprocessed/drug_consumption.csv", index=False)
     train_df, test_df = train_test_split(df, train_size=0.8, shuffle=False, random_state=522)
-    
-    train_df.to_csv(os.path.join(preprocessed_out_dir, "train.csv"), index=False)
-    test_df.to_csv(os.path.join(preprocessed_out_dir, "test.csv"), index=False)
 
+    try:
+        train_df.to_csv(os.path.join(preprocessed_out_dir, "train.csv"), index=False)
+    except:
+        os.makedirs(os.path.dirname(preprocessed_out_dir))
+        train_df.to_csv(os.path.join(preprocessed_out_dir, "train.csv"), index=False)
+
+    try:
+        test_df.to_csv(os.path.join(preprocessed_out_dir, "test.csv"), index=False)
+    except:
+        os.makedirs(os.path.dirname(preprocessed_out_dir))
+        test_df.to_csv(os.path.join(preprocessed_out_dir, "test.csv"), index=False)
+    
     # Drop drugs columns that will not be used for analysis
     train_df = train_df.drop(columns = mappings["drop"])
     test_df = test_df.drop(columns = mappings["drop"])
@@ -42,9 +68,18 @@ def main(in_path, preprocessed_out_dir, processed_out_dir):
     for col_name in mappings["categorical"] + mappings["ordinal"]:
         train_df[col_name] = train_df[col_name].str.strip()
         test_df[col_name] = test_df[col_name].str.strip()
-    
-    train_df.to_csv(os.path.join(processed_out_dir, "train.csv"), index=False)
-    test_df.to_csv(os.path.join(processed_out_dir, "test.csv"), index=False)
+
+    try:
+        train_df.to_csv(os.path.join(processed_out_dir, "train.csv"), index=False)
+    except:
+        os.makedirs(os.path.dirname(processed_out_dir))
+        train_df.to_csv(os.path.join(processed_out_dir, "train.csv"), index=False)
+
+    try:
+        test_df.to_csv(os.path.join(processed_out_dir, "test.csv"), index=False)
+    except:
+        os.makedirs(os.path.dirname(processed_out_dir))
+        test_df.to_csv(os.path.join(processed_out_dir, "test.csv"), index=False)
 
 if __name__ == "__main__":
     opt = docopt(__doc__)
