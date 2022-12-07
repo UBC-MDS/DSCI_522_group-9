4a1ffacf6cd961df6a04155d6762ec5158374572
Ritisha Sharma <ritishas2000@gmail.com>
2022-12-06 23:17:01 -0800

    Add docstring and test

--- src/download_data.py
+++ src/download_data.py
@@ -1,5 +1,9 @@
-"""This script downloads data given a URL and local file path.
+"""This script downloads data into a csv file given a URL and local file path.
+
 Usage: download_data.py --url=<url> --file_path=<file_path>
+
+Example: python src/download_data.py --url=https://archive.ics.uci.edu/ml/machine-learning-databases/00373/drug_consumption.data --file_path=data/raw
+
 Options:
 --url=<url>                 Takes in the url (this is a required option)
 --file_path=<file_path>     Takes in the local file path (this is a required option)
@@ -9,17 +13,30 @@ from docopt import docopt
 import pandas as pd
 import os
 
-def main(opt):
-    data = pd.read_csv(opt["--url"], header=None)
-    
+def main(url, file_path):
+    """
+    Downloads data from url into csv
+    Parameters
+    ----------
+    url : string
+        url from where data is read
+    file_path: string
+        path to which csv file is saved
+    Returns
+    -------
+    None
+    Example
+    --------
+    main("https://archive.ics.uci.edu/ml/machine-learning-databases/00373/drug_consumption.data", "data/raw")
+    """
+    data = pd.read_csv(url, header=None)
+    # Save results to result path
     try:
-        data.to_csv(os.path.join(opt["--file_path"] , "drug_consumption.csv"), index = False,
-        header = None)
+        data.to_csv(os.path.join(file_path , "drug_consumption.csv"), index = False)
     except:
-        os.makedirs(os.path.join(opt["--file_path"]))
-        data.to_csv(os.path.join(opt["--file_path"] , "drug_consumption.csv"), index = False,
-        header = None)
+        os.makedirs(file_path)
+        data.to_csv(os.path.join(file_path , "drug_consumption.csv"), index = False)
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
-    main(arguments)
+    main(arguments["--url"], arguments["--file_path"])
